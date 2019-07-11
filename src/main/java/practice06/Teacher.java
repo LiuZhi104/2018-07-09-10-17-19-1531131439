2e16034d9aecfd6d4a1938c35e8e1db14bba4d22
LiuZhi104 <2458461623@qq.com>
2019-07-11 20:21:09 +0800

    practice commit

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int klass) {
+        super(name);
+        this.klass = klass;
+    }
+
+    public  String introduce(){
+        if(klass!=2){
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+        return "My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.";
+    }
+
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+    private  int klassone;
+    private  Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int klassone) {
+        super(name);
+        this.klassone = klassone;
+    }
+
+    public  String introduce(){
+        if(klassone!=2 || (klass.getNumber()!=2)){
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+        return "My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.";
+    }
+   public String introduceWith( Student student){
+        return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.";
+   }
+
+
 }
